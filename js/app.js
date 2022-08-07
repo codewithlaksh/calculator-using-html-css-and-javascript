a5093d400d0b6b0369b77471b40de1df14885baf
codewithlaksh <dashlakshyaraj2006@gmail.com>
2022-08-07 09:37:12 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// JavaScript Logic
+let buttons = document.querySelectorAll('button');
+let screen = document.querySelector('#screen');
+Array.from(buttons).forEach(element => {
+    element.addEventListener('click', (e) => {
+        if (e.target.id == 'resetBtn') {
+            screen.value = "";
+        }
+        else if (e.target.id == 'deleteChar') {
+            let output = screen.value.substring(0, screen.value.length - 1);
+            screen.value = output;
+        }
+        else if (e.target.id == 'evaluate') {
+            let output = eval(screen.value);
+            screen.value = output;
+        }
+        else {
+            screen.value += e.target.innerText;
+        }
+    })
+})
